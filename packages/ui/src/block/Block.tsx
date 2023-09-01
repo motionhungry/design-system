c94a8b5e9a79f77f5474411fd2f2d47d767dedb1
Kyle Getrost <kyle@motionhungry.com>
2023-09-01 20:28:43 +0700

    fix(ui): add background color and color props to Block component

--- packages/ui/src/block/Block.tsx
+++ packages/ui/src/block/Block.tsx
@@ -1,22 +1,27 @@
 import { assignInlineVars } from '@vanilla-extract/dynamic';
 import { Box } from '@/box';
 import { Container } from '@/container';
-
+import { Sprinkles } from '@/box/Box.sprinkles.css';
 import { className, minHeight } from './Block.styles.css';
 
 type BlockProps = {
   backgroundImage?: string;
   children: React.ReactNode;
   fullHeight?: boolean;
-};
+} & Pick<Sprinkles, 'backgroundColor' | 'bgColor' | 'color'>;
 
-const Block = ({ children, fullHeight = false }: BlockProps): JSX.Element => {
+const Block = ({
+  children,
+  fullHeight = false,
+  ...props
+}: BlockProps): JSX.Element => {
   return (
     <Box
       className={className}
       style={assignInlineVars({
         [minHeight]: fullHeight ? '100vh' : 'auto',
       })}
+      {...props}
     >
       <Container>{children}</Container>
     </Box>
